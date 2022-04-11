19c056b221eeb1575566658a18c7079cb7d5b9f6
Aman <icrextha@gmail.com>
2022-04-11 22:17:46 +0545

    Added my project

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor('###8190cd');
+final Color accentHexColor = HexColor('#181818');
+  
